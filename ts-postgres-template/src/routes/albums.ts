987ba48ba5c059ad39ac10bfcb18e11cb31926de
Sid <siddhantg2002@gmail.com>
2025-02-06 00:04:14 +0530

    added readme

---
+++ ts-postgres-template/src/routes/albums.ts
@@ -0,0 +1,22 @@
+// Purpose: Handle all routes related to albums endpoint.
+import { Router } from "express";
+import { SubRoutes } from "./Sub_Routes";
+import Data from "../utils/Data";
+import JWT from "../controllers/Authentication";
+import limiter from "../controllers/rate_limitter";
+import { prisma } from "../utils/Clients/Prisma";
+
+
+const createUserRoutes = (): Router => {
+    const auth = new JWT();
+    const albumRoutes = new SubRoutes();
+    const albums = new Data(prisma.album)
+
+    albumRoutes.endpoint('get', '/', albums.getAll.bind(albums), [auth.decryptJWT, limiter]);
+    albumRoutes.endpoint('get', '/:id', albums.getOne.bind(albums), [auth.decryptJWT, limiter]);
+    
+    return albumRoutes.getRouter();
+};
+
+const users = createUserRoutes()
+export default users;
