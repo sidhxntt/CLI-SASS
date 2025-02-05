1f3018da597cfbedf01a3c9e0035c59977898be5
Sid <siddhantg2002@gmail.com>
2025-02-05 13:17:21 +0530

    fixes

--- src/utils/cancelOperation.ts
+++
@@ -1,9 +0,0 @@
-import * as p from "@clack/prompts";
-import color from "picocolors";
-
-export default function cancelOperation(input: unknown) {
-  if (p.isCancel(input)) {
-    p.outro(`${color.bgRed(color.white(`Operation Cancelled`))}`);
-    process.exit(0);
-  }
-}
